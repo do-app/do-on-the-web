59dcda4b88bc0585fb8cae045d365c185c5c4e2b
Anna Karingal <anna.karingal@gmail.com>
2015-09-18 19:56:26 -0400

    add user chores relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :household
   has_many :events
+  has_many :user_chores
+  has_many :chores, through: :user_chores, source: :user
 end
--- app/models/user_chore.rb
+++ app/models/user_chore.rb
@@ -1,3 +1,5 @@
 class UserChore < ActiveRecord::Base
   self.table_name = 'users_chores'
+
+  belongs_to :user
 end
