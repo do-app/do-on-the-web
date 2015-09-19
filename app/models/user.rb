6beddbe96ea8234a0452e39e4889f63b3fbe07d9
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 12:13:16 -0400

    validates email for uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :user_chores
   has_many :chores, through: :user_chores, source: :chore
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :points, presence: true
   validates :household, presence: true
