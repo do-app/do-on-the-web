79c97b8391b2bcc3bd27bc9089624cd526eea363
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 12:13:44 -0400

    validate email for format

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :chores, through: :user_chores, source: :chore
 
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :name, presence: true
   validates :points, presence: true
   validates :household, presence: true
