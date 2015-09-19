8589d4cb6e0028422ed48f754ff63b910dce09ab
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 12:04:09 -0400

    add head of household relation

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,4 +1,6 @@
 class Household < ActiveRecord::Base
   has_many :users
   has_many :chores
+  belongs_to :head_of_household, class_name: 'User',
+                                  foreign_key: 'head_of_household_id'
 end
