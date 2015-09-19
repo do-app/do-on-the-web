34152f799b0530f32e3efb8cbf2ce1f886232eaa
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 12:09:47 -0400

    validate all fields for presence

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,4 +2,10 @@ class Chore < ActiveRecord::Base
   belongs_to :household
   has_many :user_chores
   has_many :users, through: :user_chores, source: :user
+
+  validates :name, presence: true
+  validates :points, presence: true
+  validates :length_of_time, presence: true
+  validates :times_per_week, presence: true
+  validates :household, presence: true
 end
