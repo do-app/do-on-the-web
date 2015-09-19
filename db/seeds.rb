4cafcb6e6ab4d99dcfe32e12502458a079f18676
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 12:04:19 -0400

    seed dev database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+CHORES = {
+  # Chore name => [points, times_per_week]
+  "Do the laundry" => [100,1],
+  "Walk the dog" => [5, 7],
+  "Feed the fish" => [10, 3],
+  "Clean the bathroom" => [150, 1],
+  "Do the dishes" => [5, 14], 
+  "Vacuum" => [100, 2], 
+  "Mop" => [100, 2], 
+  "Feed the dog" => [2, 14], 
+  "Do the grocery shopping" => [100, 1], 
+  "Clean the kitchen" => [100, 3], 
+  "Clean the toilet" => [200, 1], 
+  "Wipe down the counter" => [50, 7], 
+  "Make the bed" => [10, 7]
+}
+
+20.times do 
+  User.create(
+      name: Faker::Name.name,
+      email: Faker::Internet.email
+    )
+end
+
+5.times do |user_id|
+  household = Household.create(
+      name: Faker::Team.name,
+      head_of_household_id: user_id + 1
+    )
+  CHORES.keys.each do |chore_name| 
+    household.chores << Chore.new(
+                              name: CHORES[chore_name],
+                              points: CHORES[chore_name][0],
+                              times_per_week: CHORES[chore_name][1]
+                            )
+  end
+  household.save
+end
+
+NUM_HOUSEHOLDS = Household.count
+
+User.all.each do |user|
+  unless user.household
+    user.household_id = rand(1..NUM_HOUSEHOLDS-1)
+    user.save
+  end
+  rand(1..3).times do 
+    user.chores << user.household.chores.sample
+  end
+end
