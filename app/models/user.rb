f2009f8cc7c0997df5da5a69d996d4961dd23dd0
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 12:03:44 -0400

    fix-user chores relation bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   belongs_to :household
   has_many :events
   has_many :user_chores
-  has_many :chores, through: :user_chores, source: :user
+  has_many :chores, through: :user_chores, source: :chore
 end
