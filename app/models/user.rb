f01629bcbffffe4dc6092ccc99e52a21cd2b61f5
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 12:11:53 -0400

    validates all fields for presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :user_chores
   has_many :chores, through: :user_chores, source: :chore
+
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :points, presence: true
+  validates :household, presence: true
 end
