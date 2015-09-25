ff073dee90b92a85132339832d15ef33f2a04fbd
Anna Karingal <anna.karingal@gmail.com>
2015-09-24 21:21:13 -0400

    add password col to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925005609) do
+ActiveRecord::Schema.define(version: 20150925012036) do
 
   create_table "chores", force: :cascade do |t|
     t.string   "name",           limit: 255, null: false
@@ -39,12 +39,13 @@ ActiveRecord::Schema.define(version: 20150925005609) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",         limit: 255, null: false
-    t.string   "email",        limit: 255, null: false
-    t.integer  "points",       limit: 4,   null: false
-    t.integer  "household_id", limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name",            limit: 255, null: false
+    t.string   "email",           limit: 255, null: false
+    t.integer  "points",          limit: 4,   null: false
+    t.integer  "household_id",    limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
   create_table "users_chores", force: :cascade do |t|
