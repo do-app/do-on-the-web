ba10d853d601ff49b8a4f1b7c0b41d8646c89598
Anna Karingal <anna.karingal@gmail.com>
2015-09-25 13:18:19 -0400

    add root & dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'dashboard#index'
+  resources :dashboard, only: [:index]
   resources :users, except: [:index]
   resources :households, except: [:index] do 
     resources :chores
