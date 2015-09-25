c23289d773d812f1ba3764f8fbfae1847ade028e
Anna Karingal <anna.karingal@gmail.com>
2015-09-25 11:23:21 -0400

    remove household validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :name, presence: true
   validates :points, presence: true
-  validates :household, presence: true
 end
