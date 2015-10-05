275ee13b9e6916ba9dd198ad301f2cf97ea38d60
Anna Karingal <anna.karingal@gmail.com>
2015-10-05 10:41:13 -0400

    set completed flag to false by default

--- app/models/user_chore.rb
+++ app/models/user_chore.rb
@@ -4,7 +4,14 @@ class UserChore < ActiveRecord::Base
   belongs_to :user
   belongs_to :chore 
 
+  before_validation :set_completed
+
   validates :user, presence: true
   validates :chore, presence: true
   validates :completed, presence: true
+
+  private
+  def set_completed
+    self.completed ||= false
+  end
 end
