40585351751e455f049eb9c65bec395a421ad9cc
Anna Karingal <anna.karingal@gmail.com>
2015-10-05 10:40:25 -0400

    fix bugs in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,27 @@
 CHORES = {
   # Chore name => [points, times_per_week]
-  "Do the laundry" => [100,1],
-  "Walk the dog" => [5, 7],
-  "Feed the fish" => [10, 3],
-  "Clean the bathroom" => [150, 1],
-  "Do the dishes" => [5, 14], 
-  "Vacuum" => [100, 2], 
-  "Mop" => [100, 2], 
-  "Feed the dog" => [2, 14], 
-  "Do the grocery shopping" => [100, 1], 
-  "Clean the kitchen" => [100, 3], 
-  "Clean the toilet" => [200, 1], 
-  "Wipe down the counter" => [50, 7], 
-  "Make the bed" => [10, 7]
+  "Do the laundry" => [100,1,60],
+  "Walk the dog" => [5, 7, 15],
+  "Feed the fish" => [10, 3,5],
+  "Clean the bathroom" => [150, 1,30],
+  "Do the dishes" => [5, 14,10], 
+  "Vacuum" => [100, 2,45], 
+  "Mop" => [100, 2,45], 
+  "Feed the dog" => [2, 14,5], 
+  "Do the grocery shopping" => [100, 1,90], 
+  "Clean the kitchen" => [100, 3,30], 
+  "Clean the toilet" => [200, 1,15], 
+  "Wipe down the counter" => [50, 7,10], 
+  "Make the bed" => [10, 7,5]
 }
 
 20.times do 
-  User.create(
+  u = User.new(
       name: Faker::Name.name,
       email: Faker::Internet.email
     )
+  u.password = "password"
+  u.save
 end
 
 5.times do |user_id|
@@ -27,11 +29,12 @@ end
       name: Faker::Team.name,
       head_of_household_id: user_id + 1
     )
-  CHORES.keys.each do |chore_name| 
-    household.chores << Chore.new(
+  CHORES.keys.each do |chore_name|
+    household.chores << Chore.create(
                               name: CHORES[chore_name],
                               points: CHORES[chore_name][0],
-                              times_per_week: CHORES[chore_name][1]
+                              times_per_week: CHORES[chore_name][1], 
+                              length_of_time: CHORES[chore_name][2]
                             )
   end
   household.save
@@ -45,6 +48,9 @@ User.all.each do |user|
     user.save
   end
   rand(1..3).times do 
-    user.chores << user.household.chores.sample
+    chores = user.household.chores.sample(4)
+    chores.each do |chore| 
+      user.chores << chore
+    end
   end
 end
