7ea2d4299fc81b7b8c4c56dbf79c55a002ad632c
Anna Karingal <anna.karingal@gmail.com>
2015-10-05 11:33:44 -0400

    Household has members not users

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,5 +1,5 @@
 class Household < ActiveRecord::Base
-  has_many :users
+  has_many :members, class_name: 'User'
   has_many :chores
   belongs_to :head_of_household, class_name: 'User',
                                   foreign_key: 'head_of_household_id'
