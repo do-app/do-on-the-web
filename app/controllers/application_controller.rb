83dd54f9f851537ebfe239db3088e8ad4d1a27b5
Anna Karingal <anna.karingal@gmail.com>
2015-10-05 10:42:52 -0400

    implement methods for authenticaiton

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def is_authenticated?
+    !!session[:user_id]
+  end
+
+  def authenticate
+    unless is_authenticated? 
+      flash[:errors] = ["You must be logged in to perform this action"]
+      redirect_to root_path
+    end
+  end
 end
