b376c451ce4dfb75e45652ebf3bb012476b84c20
Anna Karingal <anna.karingal@gmail.com>
2015-10-19 11:36:33 -0400

    setup DatabaseCleaner and FactoryGirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,8 +14,20 @@ Dir[Rails.root.join("spec/support/*.rb")].each {|f| require f}
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include FactoryGirl::Syntax::Methods
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  # config.include LoginMacros
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
