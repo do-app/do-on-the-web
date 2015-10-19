f8af0c001c91e7fd894b7945da8ac2f849675cbd
Anna Karingal <anna.karingal@gmail.com>
2015-10-19 11:37:15 -0400

    test for household validations

---
+++ spec/models/households_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Household do 
+  before (:each) do
+    @user = create(:user)
+    @household = create(:household, head_of_household: @user)
+    @user.household = @household
+  end
+
+  it "is valid with a head of household, members and no chores" do 
+    expect(@household).to be_valid
+  end
+
+  it "is invalid with no name" do 
+    @household.name = ''
+    expect(@household).to be_invalid
+  end
+
+  it "is invalid with no head of household" do 
+    @household.head_of_household = nil
+    expect(@household).to be_invalid
+  end
+
+  it "returns the correct household when searched by name" do
+    result = Household.search(@household.name)
+    expect(result[0]).to eq(@household)
+  end
+end
