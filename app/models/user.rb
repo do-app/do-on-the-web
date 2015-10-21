2374478b1b0786be1d6ee7a1a77dc53921898f11
Anna Karingal <anna.karingal@gmail.com>
2015-10-20 23:05:53 -0400

    add households_is_head_of association for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :user_chores
   has_many :chores, through: :user_chores, source: :chore
+  has_many :households_is_head_of, class_name: 'Household', foreign_key: 'head_of_household_id'
 
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
