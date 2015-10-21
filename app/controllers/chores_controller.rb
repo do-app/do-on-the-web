8c5148a95d8477c69f43748bcf00680e74b8c573
Anna Karingal <anna.karingal@gmail.com>
2015-10-20 23:06:42 -0400

    Skeleton dat controller

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,2 +1,31 @@
 class ChoresController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy 
+  end
+
+  private
+  def chore_params
+    params.require(:chore).permit(:name,
+                                  :points,
+                                  :length_of_time,
+                                  :times_per_week)
+  end
 end
