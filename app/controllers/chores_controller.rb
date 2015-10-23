07c1a579d210262d8986d9d970e37d2a37cff402
Anna Karingal <anna.karingal@gmail.com>
2015-10-23 18:21:40 -0400

    implement the new chore route and write tests

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,6 +1,19 @@
 class ChoresController < ApplicationController
 
+  before_action :authenticate
+
   def new
+    @household = Household.find_by(id: params[:household_id])
+    if current_user.household == @household
+      @chore = Chore.new
+    else 
+      flash[:error] = "You must be a member of this household to add a new chore!"
+      if current_user.household
+        redirect_to current_user.household
+      else 
+        redirect_to households_path
+      end
+    end
   end
 
   def create
--- spec/controllers/chores_controller_spec.rb
+++ spec/controllers/chores_controller_spec.rb
@@ -4,7 +4,10 @@ describe ChoresController do
   before :each do 
     @test_head_of_household = create(:user)
     @test_member = create(:user)
-    @test_household = create(:household, )
+    @test_non_member = create(:user)
+    @test_household = create(:household, head_of_household: @test_head_of_household)
+    @test_household.members << @test_member
+    @test_household.members << @test_head_of_household
   end
 
   describe 'head of household access' do
@@ -19,6 +22,23 @@ describe ChoresController do
       stub_current_user(@test_member)
       stub_authorize_user!
     end
+
+    describe 'GET#new' do 
+      it 'assigns a the correct household to @household' do 
+        get :new, household_id: @test_household
+        expect(assigns[:household]).to eq @test_household
+      end
+
+      it 'assigns a new chore to @chore' do 
+        get :new, household_id: @test_household
+        expect(assigns[:chore]).to be_a_new(Chore)
+      end
+
+      it 'renders the new template' do 
+        get :new, household_id: @test_household
+        expect(response).to render_template :new
+      end
+    end
   end
 
   describe 'non member access' do 
@@ -26,11 +46,28 @@ describe ChoresController do
       stub_current_user(@test_non_member)
       stub_authorize_user!
     end
+
+    describe 'GET#new' do 
+      it 'redirects to the users own household page' do
+        get :new, household_id: @test_household
+        expect(response).to redirect_to households_path
+      end
+
+      it 'renders a flash error' do 
+        get :new, household_id: @test_household
+        expect(flash[:error]).to be_present
+      end
+    end
   end
 
   describe 'guest access' do 
-    describe 'GET#index' do 
+    describe 'GET#new' do 
+      it 'redirects to the home page' do 
+        get :new, household_id: @test_household
+        expect(response).to redirect_to root_path
+      end
     end
+
   end
 
 end
