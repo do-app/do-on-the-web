a2b553ebaccd300cdaab2b83717fb0f9e5470727
Anna Karingal <anna.karingal@gmail.com>
2015-10-31 18:04:28 -0400

    implement methods to get chores per member, assigned/unansiigned unexpired chores, assign any unassigned chores

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,4 +1,5 @@
 class Household < ActiveRecord::Base
+  include AssignmentPeriod
   has_many :members, class_name: 'User'
   has_many :chores
   belongs_to :head_of_household, class_name: 'User',
@@ -7,7 +8,37 @@ class Household < ActiveRecord::Base
   validates :name, presence: true
   validates :head_of_household, presence: true
 
+
   def self.search(query)
     where("name like ?", "%#{query}%") 
   end
+
+  def chores_per_member
+     chores.count / members.count
+  end
+
+  def assigned_chores
+    chores.joins(:user_chores)
+      .where("users_chores.created_at >= ?", start_of_period)
+      .group('chores.id')
+  end
+
+  # TODO: Speed this query up with smarter query statement
+  def unassigned_chores
+    chores - assigned_chores
+  end
+
+  def assign_chores 
+    if unassigned_chores
+      unassigned_chores.each do |chore|
+        u = members.sample
+        while u.chores.count >= chores_per_member do 
+          u = members.sample
+        end 
+        u.chores << chore
+        u.save
+      end
+    end
+  end
+
 end
