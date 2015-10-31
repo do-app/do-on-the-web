d33f553d9b7ae183c9e381a56e4585a69cbbd102
Anna Karingal <anna.karingal@gmail.com>
2015-10-31 19:03:32 -0400

    test if household returns chores per member, assigned/unassigned unexpired chores

--- spec/models/households_spec.rb
+++ spec/models/households_spec.rb
@@ -4,6 +4,12 @@ describe Household do
   before (:each) do
     @user = create(:user)
     @household = create(:household, head_of_household: @user)
+    @chore = create(:chore, household: @household)
+    5.times do 
+      create(:chore, household: @household)
+    end
+    @user2 = create(:user, household: @household)
+    @user2.chores << @chore
   end
 
   it "is valid with a head of household, members and no chores" do 
@@ -27,5 +33,17 @@ describe Household do
   it "returns the correct household when searched by name" do
     result = Household.search(@household.name)
     expect(result[0]).to eq(@household)
+  end
+
+  it "returns the correct number of chores per member" do 
+    expect(@household.max_chores_per_member).to eq 3
+  end
+
+  it "returns chores that have been assigned that week" do 
+    expect(@household.assigned_chores).to eq [@chore]
+  end
+
+  it "returns chores that have not yet been assigned for the week" do 
+    expect(@household.unassigned_chores).not_to include @chore
   end
 end
