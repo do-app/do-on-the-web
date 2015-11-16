e5baea9957931f8aeca809f86f0db50aea3a8040
Anna Karingal <anna.karingal@gmail.com>
2015-11-16 11:51:15 -0500

    set up sidekiq & clockwork for production

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,7 +1,18 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
-end
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'ENV["REDIS_URL"]' }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'ENV["REDIS_URL"]' }
+  end
+  
+else
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379/12' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://localhost:6379/12' }
+  end
 end
