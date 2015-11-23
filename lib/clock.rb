68bc06706d42fc8c49d3f50430440972e11c7837
Anna Karingal <anna.karingal@gmail.com>
2015-11-23 11:17:11 -0500

    sched chores every tues instead of sunday (for testing in prod)

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,7 +3,7 @@ Dir["/jobs/*.rb"].each { |f| require f }
 require 'clockwork'
 
 module Clockwork
-  every(1.week, 'assign_chores.every_sunday', at ='Sunday 00:00') do
+  every(1.week, 'assign_chores.every_sunday', at ='Tuesday 00:00') do
     Household.all.each do |household| 
       AssignRemainingChoresJob.perform_later(household.id)
     end
