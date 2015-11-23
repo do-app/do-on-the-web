d5d4cd2f09b19a79eee844bbfee0159f8ca88ced
Anna Karingal <anna.karingal@gmail.com>
2015-11-23 11:17:45 -0500

    fix env vars typo

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,13 +1,14 @@
 if Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: 'ENV["REDIS_URL"]' }
+    config.redis = { url: ENV["REDIS_URL"] }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'ENV["REDIS_URL"]' }
+    config.redis = { url: ENV["REDIS_URL"] }
   end
-  
-else
+end
+
+if Rails.env.development?
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379/12' }
   end
