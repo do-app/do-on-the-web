38e1dad7aef7853aa2d7b415550868f42c8ff320
Anna Karingal <anna.karingal@gmail.com>
2015-12-01 14:36:29 -0500

    user chores returns only chores assigned for current period

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include AssignmentPeriod
+
   has_secure_password
   before_validation :set_points
 
@@ -6,15 +8,20 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :user_chores
   has_many :messages
-  has_many :chores, through: :user_chores, source: :chore
+  has_many :assigned_chores, through: :user_chores, source: :chore
   has_many :households_is_head_of, class_name: 'Household', foreign_key: 'head_of_household_id'
-
 
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, confirmation: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates :password, confirmation: true 
   validates :name, presence: true
+
+  def chores
+    assigned_chores.joins(:user_chores)
+      .where("users_chores.created_at >= ?", start_of_period)
+      .group('chores.id')
+  end
 
   private
     def set_points
