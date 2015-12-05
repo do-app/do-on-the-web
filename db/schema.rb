aee31c296e622a42626230a31ff768253fe05540
Anna Karingal <anna.karingal@gmail.com>
2015-12-05 18:13:46 -0500

    create reward model & migrate with validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119043937) do
+ActiveRecord::Schema.define(version: 20151205230722) do
 
   create_table "chores", force: :cascade do |t|
     t.string   "name",           limit: 255, null: false
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 20151119043937) do
     t.integer  "household_id", limit: 4
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.string   "name",         limit: 255, null: false
+    t.integer  "points",       limit: 4,   null: false
+    t.integer  "household_id", limit: 4,   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade do |t|
