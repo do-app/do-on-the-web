bb3c1ecc63e660673546200df3bfa92709f14f97
Anna Karingal <anna.karingal@gmail.com>
2015-12-06 11:24:37 -0500

    nest rewards under households

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,17 @@ Rails.application.routes.draw do
       put 'assign', on: :member
       put 'complete', on: :member
     end
+
+    resources :rewards, except: [:index, :show] do 
+      put 'claim', on: :member
+    end
+    
     put 'join', on: :member
     put 'leave', on: :member
     get 'search', on: :collection
     get 'results', on: :collection
   end
 
-  resources :rewards, except: [:index, :show] do 
-    put 'claim', on: :member
-  end
   resources :events
   resources :sessions, only: [:new, :create, :destroy]
   resources :messages
