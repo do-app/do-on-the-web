2784a39fec95b1e464242bb870737f2518ec3223
Anna Karingal <anna.karingal@gmail.com>
2015-12-11 18:05:29 -0500

    fix bug with claiming rewards not subtracting points

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -48,22 +48,23 @@ class RewardsController < ApplicationController
   def claim
     household = Household.find_by(id: params[:household_id])
     reward = Reward.find_by(id: params[:id])
+    user = current_user
     redirect_to household and return unless belongs_to_household? reward, household
-    if current_user.points < reward.points
+    if user.points < reward.points
       flash[:error] = "You don't have enough points to claim this reward"
       redirect_to :back
     end
     if validate_current_user_belongs_to_household (household)
-      if current_user.claimed_rewards << reward
-        current_user.points -= reward.points
-        if current_user.save
+      if user.claimed_rewards << reward
+        user.points -= reward.points
+        if user.save
           flash[:success] = "You have claimed your reward: #{reward.name} for #{reward.points} points!"
           redirect_to :back
         else
-          flash[:errors] = current_user.errors.full_messages
+          flash[:errors] = user.errors.full_messages
         end
       else 
-        flash[:errors] = current_user.errors.full_messages
+        flash[:errors] = user.errors.full_messages
         redirect_to :back
       end
     end  
